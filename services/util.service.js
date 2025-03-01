0ee8c4cd969446c6e1905973a75af8a23b22387a
andrio <andrio1hanania@gmail.com>
2025-03-01 22:38:53 +0200

    create _initDataFiles

--- services/util.service.js
+++ services/util.service.js
@@ -1,8 +1,21 @@
 import fs from 'fs'
+import path from 'path';
+
+_initDataFiles();
+
+function _initDataFiles() {
+    const dataDir = path.join(__dirname, 'data');
+
+    // Check if /data directory exists, if not, create it
+    if (!fs.existsSync(dataDir)) {
+        fs.mkdirSync(dataDir, { recursive: true });
+        console.log('Created /data directory');
+    }
+}
 
 export function readJsonFile(path) {
     if (!fs.existsSync(path)) {
-        fs.writeFileSync(path, JSON.stringify({}, null, 2)); // Create an empty JSON file
+        fs.writeFileSync(path, "[]", 'utf8'); // Create an empty JSON file
     }
 
     const json = fs.readFileSync(path, 'utf8');
@@ -13,7 +26,7 @@ export function writeJsonFile(path, data) {
     return new Promise((resolve, reject) => {
         // Ensure the file exists by creating it if necessary
         if (!fs.existsSync(path)) {
-            fs.writeFileSync(path, JSON.stringify({}, null, 2));
+            fs.writeFileSync(path, "[]");
         }
 
         const json = JSON.stringify(data, null, 4);
