90c4f5e8d1c49e8d9afc6935fa34c8b0d26aed4d
andrio <andrio1hanania@gmail.com>
2025-03-01 23:24:26 +0200

    add tls to mongo

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 
 export default {
-    dbURL: process.env.MONGO_URL || 'mongodb+srv://andrio1hanania:ayk5FIq035QN26bo@cluster0.gxk5p.mongodb.net/',
+    dbURL: process.env.MONGO_URL || 'mongodb+srv://andrio1hanania:<db_password>@cluster0.gxk5p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true',
     dbName: process.env.DB_NAME || 'miss-bugs-prod'
 }
